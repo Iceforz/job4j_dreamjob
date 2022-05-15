58951e879ae5b82250b15643a5a4c3e9268ce533
iceforz <iceforz3@gmail.com>
2022-05-15 11:43:16 +0300

    4. Thymeleaf, Циклы. [#504841]

---
+++ src/main/java/dream/control/PostControl.java
@@ -0,0 +1,19 @@
+package dream.control;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import dream.store.PostStore;
+
+
+@Controller
+public class PostControl {
+
+    private final PostStore store = PostStore.instOf();
+
+    @GetMapping("/posts")
+    public String posts(Model model) {
+        model.addAttribute("posts", store.findAll());
+        return "posts";
+    }
+}
--- src/main/java/dream/controller/IndexControl.java
+++ src/main/java/dream/controller/IndexControl.java
@@ -1,4 +1,4 @@
-package ru.job4j.dreamjob.controller;
+package dream.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
