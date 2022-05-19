16dea29c75bf61605a63dec7454bc2e28fb14854
iceforz <iceforz3@gmail.com>
2022-05-19 11:35:52 +0300

    5. Создания и редактирования кандидатов. [#504858]

--- src/main/java/ru/job4j/dreamjob/controller/CandidateControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/CandidateControl.java
@@ -3,7 +3,11 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.CandidateStore;
 
 
@@ -19,9 +23,27 @@ public class CandidateControl {
     }
 
        @GetMapping("/formAddCandidate")
-    public String addPost(Model model) {
+    public String addCandidate(Model model) {
         model.addAttribute("candidate",
                 new Candidate(0, "Заполните поле", "Заполните поле", null));
         return "addCandidate";
+    }
+
+    @PostMapping("/createCandidate")
+    public String createCandidate(@ModelAttribute Candidate candidate) {
+        store.add(candidate);
+        return "redirect:/candidates";
+    }
+
+    @GetMapping("/formUpdateCandidate")
+    public String updateCandidate(Model model, @PathVariable("candidateID")int id) {
+        model.addAttribute("candidate", store.findById(id));
+        return "updateCandidate";
+    }
+
+    @PostMapping("/updateCandidate")
+    public String updateCandidate(@ModelAttribute Candidate candidate) {
+        store.updateCandidate(candidate);
+        return "redirect:/candidates";
     }
 }
--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -1,11 +1,13 @@
 package ru.job4j.dreamjob.store;
 
 import ru.job4j.dreamjob.model.Candidate;
+import ru.job4j.dreamjob.model.Post;
 
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class CandidateStore {
 
@@ -13,11 +15,27 @@ public class CandidateStore {
 
     private final Map<Integer, Candidate> candidates = new ConcurrentHashMap<>();
 
+    private final static AtomicInteger CANDIDATE_ID = new AtomicInteger(3);
+
     private CandidateStore() {
         candidates.put(1, new Candidate(1, "Junior Java Job", "specialist", LocalDate.of(2022, 5, 15)));
         candidates.put(2, new Candidate(2, "Middle Java Job", "specialist", LocalDate.of(2022, 5, 15)));
     candidates.put(3, new Candidate(3, "Senior Java Job", "specialist", LocalDate.of(2022, 5, 15)));
     }
+
+    public void add(Candidate candidate) {
+        candidates.put(candidate.getId(), candidate);
+        candidate.setId(CANDIDATE_ID.incrementAndGet());
+    }
+
+    public Candidate findById(int id) {
+        return candidates.get(id);
+    }
+
+    public void updateCandidate(Candidate candidate) {
+        candidates.replace(candidate.getId(), candidate);
+    }
+
 
     public static CandidateStore instOf() {
         return INST;
