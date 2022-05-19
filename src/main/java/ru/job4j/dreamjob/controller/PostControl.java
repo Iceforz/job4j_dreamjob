78483a4a774124454286e5ee31b9f405430f94c0
iceforz <iceforz3@gmail.com>
2022-05-19 15:11:39 +0300

    2. Формы. Списки. [#504854]

--- src/main/java/ru/job4j/dreamjob/controller/PostControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostControl.java
@@ -8,45 +8,48 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
+import ru.job4j.dreamjob.sevices.CityService;
 import ru.job4j.dreamjob.sevices.PostService;
 
 @ThreadSafe
 @Controller
 public class PostControl {
 
-    private final PostService service;
+    private final PostService postservice;
+    private final CityService cityService;
 
-    public PostControl(PostService service) {
-        this.service = service;
+    public PostControl(PostService postservice, CityService cityService) {
+        this.postservice = postservice;
+        this.cityService = cityService;
     }
 
     @GetMapping("/posts")
     public String posts(Model model) {
-        model.addAttribute("posts", service.findAll());
+        model.addAttribute("posts", postservice.findAll());
         return "posts";
     }
 
     @GetMapping("/formAddPost")
     public String addPost(Model model) {
-        model.addAttribute("post", new Post(0, "Заполните поле", "Заполните поле", null));
+        model.addAttribute("cities", cityService.getAllCities());
         return "addPost";
     }
 
     @PostMapping("/createPost")
     public String createPost(@ModelAttribute Post post) {
-        service.add(post);
+        postservice.add(post);
         return "redirect:/posts";
     }
 
     @GetMapping("/formUpdatePost")
     public String updatePost(Model model, @PathVariable("postId")int id) {
-        model.addAttribute("post", service.findById(id));
+        model.addAttribute("post", postservice.findById(id));
         return "updatePost";
     }
 
     @PostMapping("/updatePost")
     public String updatePost(@ModelAttribute Post post) {
-        service.updatePost(post);
+        postservice.updatePost(post);
         return "redirect:/posts";
     }
 }
